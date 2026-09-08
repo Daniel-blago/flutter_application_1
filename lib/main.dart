1f45cbd707a18631230681e07aff3b5a7398dbc0
Daniel Blacio <anblacio@eston.ecotec.edu.ec>
2026-09-07 21:18:19 -0500

    Conectar ChangeNotifierProvider en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'screens/home_screen.dart';
+import 'providers/favorites_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => FavoritesProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
